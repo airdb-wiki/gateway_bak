50a99c10c1d6322b68a1416ee5927b06f12d7e5a
deancn <dean@airdb.com>
2022-07-21 00:01:01 +0800

    update

--- docs/.vuepress/configs/navbar/en.ts
+++ docs/.vuepress/configs/navbar/en.ts
@@ -3,83 +3,20 @@ import { version } from '../meta'
 
 export const navbarEn: NavbarConfig = [
   {
-    text: 'Guide',
-    link: '/guide/',
+    text: 'Home',
+    link: 'https://airdb.wiki/',
   },
   {
     text: 'DNS',
-    children: [
-      {
-        text: 'Bind',
-        children: [
-          { text: 'CLI', link: '/reference/cli.html', },
-        ],
-      },
-      {
-        text: 'CoreDNS',
-        children: [
-          { text: 'CaddyServer', link: '/reference/cli.html', },
-        ],
-      },
-    ],
+    link: '/dns/',
   },
   {
     text: 'Network Load Balancer',
-    children: [
-      {
-        text: '4 Layer',
-        children: [
-          '/reference/plugin/pwa.md',
-          '/reference/plugin/pwa-popup.md',
-        ],
-      },
-      {
-        text: '7 Layer',
-        children: [
-          '/reference/plugin/prismjs.md',
-          '/reference/plugin/shiki.md',
-        ],
-      },
-    ],
+    link: '/nlb/',
   },
   {
     text: 'Application Load Balancer',
-    children: [
-      {
-        text: 'Nginx',
-        children: [
-          '/advanced/architecture.md',
-          '/advanced/plugin.md',
-          '/advanced/theme.md',
-          {
-            text: 'Cookbook',
-            link: '/advanced/cookbook/',
-          },
-        ],
-      },
-      {
-        text: 'OpenResty',
-        children: [
-          '/contributing.md',
-          {
-            text: 'Awesome VuePress',
-            link: 'https://github.com/vuepress/awesome-vuepress',
-          },
-        ],
-      },
-      {
-        text: 'Caddy',
-        children: [
-          '/contributing.md',
-        ],
-      },
-      {
-        text: 'HTTPS SSL certificate',
-        children: [
-          '/contributing.md',
-        ],
-      },
-    ],
+    link: '/alb/',
   },
   {
     text: 'WAF',
@@ -89,7 +26,7 @@ export const navbarEn: NavbarConfig = [
         children: [
           {
             text: 'Cookbook',
-            link: '/advanced/cookbook/',
+            link: '/waf/',
           },
         ],
       },
