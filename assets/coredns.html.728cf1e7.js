177e82801b7b6f8951ada886b7043cf46d34e361
deancn <deancn@users.noreply.github.com>
2022-07-24 05:51:23 +0000

    deploy: cecfe2e226f7b581570419e44d6837c18cd52d87

---
+++ assets/coredns.html.728cf1e7.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-0e9c260e","path":"/dns/coredns.html","title":"CoreDNS","lang":"en-US","frontmatter":{},"excerpt":"","headers":[],"git":{"updatedTime":1658641828000,"contributors":[{"name":"deancn","email":"dean@airdb.com","commits":1}]},"filePathRelative":"dns/coredns.md"}');export{e as data};
---
+++ assets/udp.html.6138511c.js
@@ -0,0 +1,1 @@
+import{_ as a,o as t,c as o,a as e,b as r}from"./app.d84731b0.js";const c={},d=e("h1",{id:"udp-flow",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#udp-flow","aria-hidden":"true"},"#"),r(" UDP Flow")],-1),s=e("p",null,"udp replay",-1),_=[d,s];function n(l,i){return t(),o("div",null,_)}var h=a(c,[["render",n],["__file","udp.html.vue"]]);export{h as default};
